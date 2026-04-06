ffad69f47dc15dec4e6ce61103292fccf30d11fd
Petrellie <65856100+Petrellie@users.noreply.github.com>
2026-04-06 07:33:28 +0000

    Initial structure

--- budget-app/script.js
+++ budget-app/script.js
@@ -1,3 +1,11 @@
 function calculate() {
 
+  const income = Number(document.getElementById("income").value);
+  const rent = Number(document.getElementById("rent").value);
+  const food = Number(document.getElementById("food").value);
+  const transport = Number(document.getElementById("transport").value);
+  const other = Number(document.getElementById("other").value);
+
+  console.log(income, rent, food, transport, other);  
+
 }
